ce5f41bb9d65de3444dbacbe63804f57d968b898
Kaholaz <vkbugge@hotmail.com>
2023-03-19 16:43:10 +0100

    feat: permit all connections to API

---
+++ backend/src/main/java/no/ntnu/let/letapi/security/WebSecurityConfiguration.java
@@ -0,0 +1,31 @@
+package no.ntnu.let.letapi.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.web.SecurityFilterChain;
+
+/**
+ * Disable Spring Security for the application.
+ */
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfiguration {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests()
+                .anyRequest().permitAll()
+                .and()
+                .csrf().disable();
+        return http.build();
+    }
+
+    @Bean
+    public WebSecurityCustomizer webSecurityCustomizer() {
+        return web -> web.ignoring().requestMatchers(r -> true);
+    }
+}
