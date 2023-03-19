d4c54cd3d20fdcb5f12185c80eb05fc1a4898bd8
Arunan Gnanasekaran <arunan2212@gmail.com>
2023-03-19 13:30:57 +0100

    feat: add routing and 404 page

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,6 +1,5 @@
 <script setup lang="ts">
 import { RouterLink, RouterView } from "vue-router";
-import HelloWorld from "./components/HelloWorld.vue";
 </script>
 
 <template>
@@ -8,7 +7,7 @@ import HelloWorld from "./components/HelloWorld.vue";
     <div class="wrapper">
       <nav>
         <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
+        <RouterLink to="/login">login</RouterLink>
       </nav>
     </div>
   </header>
