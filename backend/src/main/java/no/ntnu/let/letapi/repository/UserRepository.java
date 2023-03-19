fc9e8ef1f890798fa550fac6e696bf05e3399913
Kaholaz <vkbugge@hotmail.com>
2023-03-19 22:47:19 +0100

    fix: made user repository public

--- backend/src/main/java/no/ntnu/let/letapi/repository/UserRepository.java
+++ backend/src/main/java/no/ntnu/let/letapi/repository/UserRepository.java
@@ -3,6 +3,6 @@ package no.ntnu.let.letapi.repository;
 import no.ntnu.let.letapi.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-interface UserRepository extends JpaRepository<User, Long> {
+public interface UserRepository extends JpaRepository<User, Long> {
     User findByEmail(String email);
 }
