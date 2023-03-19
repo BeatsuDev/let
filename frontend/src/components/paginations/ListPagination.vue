05b1695ef87adf54c71e75f406eb0f309a6518ed
Eric Bieszczad-Stie <ericbi@stud.ntnu.no>
2023-03-19 19:07:55 +0100

    chore: Added spaces for readability

--- frontend/src/components/paginations/ListPagination.vue
+++ frontend/src/components/paginations/ListPagination.vue
@@ -3,13 +3,16 @@
   <PrimaryButton :value="props.value" disabled />
   <PrimaryButton value="Neste" @click="$emit('next')" />
 </template>
+
 <script lang="ts" setup>
 import PrimaryButton from "@/components/buttons/PrimaryButton.vue";
+
 const props = defineProps({
   value: {
     type: Number,
     required: true,
   },
 });
+
 const emit = defineEmits(["next", "previous"]);
 </script>
