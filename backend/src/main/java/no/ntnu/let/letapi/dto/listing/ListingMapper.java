2441c9d832217e44cf020c0931bba307c3ad23b9
Kaholaz <vkbugge@hotmail.com>
2023-03-20 10:23:13 +0100

    fix: correctly map to listing DTOs

--- backend/src/main/java/no/ntnu/let/letapi/dto/listing/ListingMapper.java
+++ backend/src/main/java/no/ntnu/let/letapi/dto/listing/ListingMapper.java
@@ -5,6 +5,8 @@ import no.ntnu.let.letapi.model.listing.Image;
 import no.ntnu.let.letapi.model.listing.Listing;
 import no.ntnu.let.letapi.model.listing.Location;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.Mappings;
 
 import java.util.List;
 
@@ -12,17 +14,36 @@ import java.util.List;
 public interface ListingMapper {
     Location toLocation(LocationDTO locationDTO);
     LocationDTO toLocationDTO(Location location);
-    String toLocationName(Long id);
 
     Image toImage(Long id);
     ImageDTO toImageDTO(Image image);
-    String toImageUrl(Long id);
+    default String toImageUrl(Image image) {
+        ImageDTO imageDTO = toImageDTO(image);
+        return imageDTO.getUrl();
+    }
 
     String toCategoryName(Long id);
     Category toCategory(Long id);
 
+    @Mappings({
+            @Mapping(target = "thumbnail", source = "thumbnailId"),
+            @Mapping(target = "gallery", source = "galleryIds")
+    })
     Listing toListing(ListingCreationDTO listingCreationDTO);
+    @Mappings({
+            @Mapping(target = "thumbnail", source = "thumbnailId"),
+            @Mapping(target = "gallery", source = "galleryIds")
+    })
     Listing toListing(ListingUpdateDTO listingUpdateDTO);
+    @Mappings({
+            @Mapping(target = "thumbnailUrl", source = "thumbnail.url"),
+            @Mapping(target = "locationName", source = "location.name")
+    })
     ListingMinimalDTO toListingMinimalDTO(Listing listing);
+    @Mappings({
+            @Mapping(target = "thumbnailUrl", source = "thumbnail"),
+            @Mapping(target = "galleryUrls", source = "gallery"),
+            @Mapping(target = "locationName", source = "location.name")
+    })
     ListingFullDTO toListingFullDTO(Listing listing);
 }
