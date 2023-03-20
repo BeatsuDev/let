2999a5f9bb05d11be619382c2fcac70e4ba939e6
Kaholaz <vkbugge@hotmail.com>
2023-03-20 11:48:53 +0100

    fix: remove unnecessary url field from image DTO

--- backend/src/main/java/no/ntnu/let/letapi/dto/listing/ImageDTO.java
+++ backend/src/main/java/no/ntnu/let/letapi/dto/listing/ImageDTO.java
@@ -13,7 +13,6 @@ import no.ntnu.let.letapi.util.UrlUtil;
 public class ImageDTO {
     private static final String BASE_URL = UrlUtil.getBaseUrl() + "/image/";
     private long id;
-    private String url;
 
     public String getUrl() {
         return BASE_URL + id;
