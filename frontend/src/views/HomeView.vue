51c3017d331498f6cfca36a38654bdfcddc8caf4
Arunan Gnanasekaran <arunan2212@gmail.com>
2023-03-20 17:15:12 +0100

    refac: remove selfmade types and use types generated by swagger instead

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -22,28 +22,30 @@
 </template>
 <script setup lang="ts">
 import ListPagination from "@/components/paginations/ListPagination.vue";
-import { ref, watch } from "vue";
+import { ref } from "vue";
 import ListingScrollPane from "@/components/listings/ListingScrollPane.vue";
-import { Category, ListingCard, ListingFilter } from "@/types/listing";
+import { ListingFilter } from "@/types/listing";
 import NavigationDrawer from "@/components/NavigationDrawer.vue";
+import type { Category, ListingMinimal } from "@/service/models";
 
 const page = ref(1);
-const listings = ref([] as ListingCard[]);
+const listings = ref([] as ListingMinimal[]);
 const categories = ref([] as Category[]);
 const collapsed = ref(false);
 
 const listingFilter = ref(new ListingFilter());
 
-const listing = new ListingCard(
-  1,
-  "Airpods",
-  "Test men moren din er styggere ahahhah Lorem ipsum daler dat",
-  1,
-  "test",
-  "Oslo, Høybråten",
-  "test"
-);
-const category = new Category("test");
+const listing = {
+  id: 1,
+  title: "Airpods",
+  summary: "Test men moren din er styggere ahahhah Lorem ipsum daler dat",
+  price: 1,
+  thumbnailUrl: "test",
+  locationName: "Oslo, Høybråten",
+  categoryName: "test",
+} as ListingMinimal;
+
+const category = { name: "test" } as Category;
 
 for (let i = 0; i < 20; i++) {
   listings.value.push(listing);
