6ae3c32254369e158ac9de56245a52b5eb53a77e
aaarunan <arunan2212@gmail.com>
2023-03-20 23:18:01 +0100

    feat: add no matches found when listings are empty

---
+++ frontend/src/components/listings/ListingsNotFound.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="main">
+    <div class="not-found">
+      <SearchIcon style="width: 30%"/><h1>Vi fant ikke det du letet etter...</h1>
+      <p>
+        Vi fant dessverre ingen treff på søket ditt. Prøv å endre søkeordene eller
+        søkeområdet.
+      </p>
+    </div>
+  </div>
+</template>
+<style>
+.main {
+  display:flex;
+  justify-content: center;
+  align-items: center;
+  height: 100%;
+  width: 100%;
+}
+</style>
+<script setup lang="ts">
+import SearchIcon from "@/components/icons/SearchIcon.vue";
+</script>
