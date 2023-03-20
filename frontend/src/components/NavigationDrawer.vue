1661263db1b061125720e637dd34ccadd0a9e07d
Arunan Gnanasekaran <arunan2212@gmail.com>
2023-03-20 10:42:58 +0100

    feat: add homepage skeleton

---
+++ frontend/src/components/NavigationDrawer.vue
@@ -0,0 +1,79 @@
+<template>
+  <div
+    :class="{
+      sidebar: true,
+      'drawer-collapse': props.collapsed,
+      'drawer-active': !props.collapsed,
+    }"
+  >
+    <button style="margin-bottom: 1.5rem" @click="collapse">Skjul</button>
+    <div class="text-sub-header">Kategori</div>
+    <div class="text-paragraph hide-overflow">Menu Item 1</div>
+    <div class="text-paragraph hide-overflow">Menu Item 2</div>
+    <div class="text-paragraph hide-overflow">Menu Item 3</div>
+    <div class="text-sub-header">Søk</div>
+    <input type="search" v-model="value.search" />
+    <div class="text-sub-header">Lokasjon</div>
+    <input type="search" v-model="value.search" />
+    <div class="text-sub-header">radius: {{ value.radius }}</div>
+    <input type="range" v-model="value.radius" style="width: 100%" />
+  </div>
+</template>
+<script lang="ts" setup>
+import { computed } from "vue";
+import { ListingFilter } from "@/types/listing";
+
+const props = defineProps({
+  collapsed: {
+    type: Boolean,
+    default: false,
+  },
+  modelValue: {
+    type: ListingFilter,
+    required: true,
+  },
+});
+
+const emit = defineEmits(["update:modelValue", "update:collapsed"]);
+
+const value = computed({
+  get() {
+    return props.modelValue;
+  },
+  set(value: string) {
+    const newFilter = { ...props.modelValue, [value]: value };
+    emit("update:modelValue", newFilter);
+  },
+});
+
+function collapse() {
+  emit("update:collapsed", true);
+}
+</script>
+<style scoped>
+.sidebar {
+  margin-top: 4rem;
+  height: 100%;
+  position: fixed;
+  left: 0;
+  top: 0;
+  padding-top: 7.5rem;
+  padding-left: 3rem;
+  transition: width 0.5s;
+  overflow: hidden;
+  background-color: var(--color-background);
+}
+
+.drawer-active {
+  width: 15rem;
+}
+
+.drawer-collapse {
+  width: 0;
+  padding-left: 0;
+}
+
+.hide-overflow {
+  overflow: hidden;
+}
+</style>
--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -1,9 +1,92 @@
-<script setup lang="ts">
-import { getLocationsFromLongLat } from "@/service/location-api";
-
-console.log(getLocationsFromLongLat(59.9139, 10.7522));
-</script>
-
 <template>
-  <main></main>
+  <main>
+    <NavigationDrawer v-model="listingFilter" v-model:collapsed="collapsed"></NavigationDrawer>
+    <div :class="{ content: true, active: !collapsed, collapsed: collapsed }">
+      <div class="text-title one-line">Hva leter du etter i dag?</div>
+      <div class="menu">
+        <button @click="collapsed = !collapsed"></button>
+        <div class="spacer" />
+        <ListPagination :value="page" @next="nextPage" @previous="previousPage"></ListPagination>
+      </div>
+      <ListingScrollPane :listings="listings"></ListingScrollPane>
+      <div class="menu">
+        <div class="spacer" />
+        <ListPagination :value="page" @next="nextPage" @previous="previousPage"></ListPagination>
+      </div>
+    </div>
+  </main>
 </template>
+<script setup lang="ts">
+import ListPagination from "@/components/paginations/ListPagination.vue";
+import { ref } from "vue";
+import ListingScrollPane from "@/components/listings/ListingScrollPane.vue";
+import { Image, Listing, ListingFilter, Location } from "@/types/listing";
+import NavigationDrawer from "@/components/NavigationDrawer.vue";
+
+const page = ref(1);
+const listings = ref([] as Listing[]);
+const collapsed = ref(false);
+
+const listingFilter = ref(new ListingFilter());
+
+const listing = new Listing(
+  1,
+  "Test",
+  1,
+  new Date(),
+  new Date(),
+  "test",
+  "test",
+  new Image(1),
+  [new Image(1)],
+  new Location(1, 1),
+  100
+);
+
+for (let i = 0; i < 20; i++) {
+  listings.value.push(listing);
+}
+
+function nextPage() {
+  page.value++;
+}
+
+function previousPage() {
+  page.value--;
+}
+</script>
+<style scoped>
+.menu {
+  padding: 2rem;
+  display: flex;
+}
+
+.content {
+  transition: margin-left 0.5s;
+}
+
+.active {
+  margin-left: 15rem;
+}
+
+.collapsed {
+  margin-left: 5rem;
+}
+
+@media screen and (max-width: 1000px) {
+  .one-line {
+    white-space: nowrap;
+    font-size: 5vw;
+  }
+}
+
+@media screen and (max-width: 600px) {
+  .active {
+    margin-left: 0;
+  }
+}
+
+main {
+  width: 100%;
+}
+</style>
