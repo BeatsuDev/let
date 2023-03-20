b4dadb7b3a2e1d2c3efb95aca6667ab499d2e9ca
Kaholaz <vkbugge@hotmail.com>
2023-03-20 14:00:40 +0100

    feat: add user DTO mapper

---
+++ backend/src/main/java/no/ntnu/let/letapi/dto/user/UserMapper.java
@@ -0,0 +1,30 @@
+package no.ntnu.let.letapi.dto.user;
+
+import no.ntnu.let.letapi.model.user.User;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.Mappings;
+import org.mapstruct.Named;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Mapper(componentModel = "spring")
+public abstract class UserMapper {
+    @Autowired
+    PasswordEncoder passwordEncoder;
+    public abstract UserMinimalDTO toMinimalDTO(User user);
+    public abstract UserFullDTO toFullDTO(User user);
+
+    @Named("encryptPassword")
+    public String encryptPassword(String password) {
+        return passwordEncoder.encode(password);
+    }
+    @Mappings({
+            @Mapping(target = "password", source = "password", qualifiedByName = "encryptPassword")
+    })
+    public abstract User toUser(UserCreationDTO userCreationDTO);
+    @Mappings({
+            @Mapping(target = "password", source = "password", qualifiedByName = "encryptPassword")
+    })
+    public abstract User toUser(UserUpdateDTO userUpdateDTO);
+}
--- backend/src/main/java/no/ntnu/let/letapi/security/WebSecurityConfiguration.java
+++ backend/src/main/java/no/ntnu/let/letapi/security/WebSecurityConfiguration.java
@@ -5,6 +5,8 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 /**
@@ -27,5 +29,10 @@ public class WebSecurityConfiguration {
     @Bean
     public WebSecurityCustomizer webSecurityCustomizer() {
         return web -> web.ignoring().requestMatchers(r -> true);
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 }
