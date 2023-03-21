4a46f02cdf6aeca3262b9e78ef14a4fbebd5c975
Eric Bieszczad-Stie <ericbieszczad@gmail.com>
2023-03-21 08:46:43 +0100

    chore: extracted listing form to a new component

--- frontend/src/views/CreateListingView.vue
+++ frontend/src/views/CreateListingView.vue
@@ -1,60 +1,11 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import ListingCreatorForm from '@/components/ListingCreatorForm.vue';
+</script>
 
 <template>
   <div class="wrapper">
     <h1>Lag en ny annonse</h1>
-    <form>
-      <div class="row" id="row-1">
-        <div class="col">
-          <h3><label for="title">Tittel</label></h3>
-          <input class="input-text" type="text" id="title" placeholder="Rød rose - snart døende" />
-        </div>
-        <div class="col">
-          <h3><label for="price">Pris (kr)</label></h3>
-          <input class="input-text" type="text" id="price" placeholder="249.99" />
-        </div>
-      </div>
-
-      <div class="row" id="row-2">
-        <div class="col">
-          <h3><label for="place">Sted</label></h3>
-          <input class="input-text" type="text" id="place" placeholder="Kardemomme By, Norge" />
-        </div>
-        <div class="col">
-          <h3><label for="category">Kategori</label></h3>
-          <input class="input-text" type="text" id="category" placeholder="Planter" />
-        </div>
-      </div>
-
-      <div class="row" id="row-3">
-        <div class="col">
-          <h3><label for="summary">Kort beskrivelse</label></h3>
-          <textarea class="input-text" name="summary" id="summary"></textarea>
-        </div>
-      </div>
-
-      <div class="row" id="row-4">
-        <div class="col">
-          <h3><label for="description">Detaljert beskrivelse</label></h3>
-          <textarea class="input-text" name="description" id="description"></textarea>
-        </div>
-      </div>
-
-      <div class="row" id="row-5">
-        <div class="col">
-          <h3><label for="images">Last opp bilder</label></h3>
-          <input class="input-text" type="file" id="images" multiple />
-        </div>
-      </div>
-
-      <div class="row" id="row-6">
-        <div class="col">
-          <button class="button button-black button-screaming" type="submit">
-            Publiser annonse
-          </button>
-        </div>
-      </div>
-    </form>
+    <ListingCreatorForm />
   </div>
 </template>
 
@@ -64,58 +15,5 @@
   margin: 0 auto;
   min-width: 400px;
   padding: 2rem 5rem;
-}
-
-label {
-  display: block;
-  margin-bottom: 0.2rem;
-}
-
-input,
-textarea {
-  width: 100%;
-  resize: none;
-}
-
-.col {
-  padding: 0 0.4rem;
-}
-
-.row {
-  display: grid;
-  grid-template-columns: 1fr 1fr 1fr 1fr 1fr;
-  margin-bottom: 1rem;
-}
-
-.row > .col {
-  grid-column: 1 / 6;
-}
-
-#row-1 > .col:first-child {
-  grid-column: 1 / 5;
-}
-
-#row-1 > .col:last-child {
-  grid-column: 5 / 5;
-}
-
-#row-2 > .col:first-child {
-  grid-column: 1 / 4;
-}
-
-#row-2 > .col:last-child {
-  grid-column: 4 / 6;
-}
-
-#row-6 > .col:first-child {
-  grid-column: 6 / 6;
-}
-
-#description {
-  height: 200px;
-}
-
-#images {
-  cursor: pointer;
 }
 </style>
