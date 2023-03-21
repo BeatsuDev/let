0f2b0f889d181610ca4afacbb094445389744ee8
Arunan Gnanasekaran <90697695+aaarunan@users.noreply.github.com>
2023-03-21 16:14:50 +0100

    Revert "refac: componentize navigation drawer"
    This reverts commit b450f7fa66e7cfc7de735b05df30516874f141d8.

--- frontend/src/components/icons/FilterIcon.vue
+++ frontend/src/components/icons/FilterIcon.vue
@@ -2,7 +2,6 @@
   <svg class="IconSearchDropdownstyles__PreviewIcon-sc-v0m922-1 dZDKpM" viewBox="0 0 24 24">
     <path d="M0 0h24v24H0z" fill="none"></path>
     <path
-      d="M3 17v2h6v-2H3zM3 5v2h10V5H3zm10 16v-2h8v-2h-8v-2h-2v6h2zM7 9v2H3v2h4v2h2V9H7zm14 4v-2H11v2h10zm-6-4h2V7h4V5h-4V3h-2v6z"
-    ></path>
+        d="M3 17v2h6v-2H3zM3 5v2h10V5H3zm10 16v-2h8v-2h-8v-2h-2v6h2zM7 9v2H3v2h4v2h2V9H7zm14 4v-2H11v2h10zm-6-4h2V7h4V5h-4V3h-2v6z"></path>
   </svg>
 </template>
