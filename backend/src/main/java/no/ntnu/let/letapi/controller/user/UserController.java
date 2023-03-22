d53ccf6c746d0aab8943d9b3608a23645d5baa25
Kaholaz <vkbugge@hotmail.com>
2023-03-22 21:21:46 +0100

    feat: add GET /user/session endpoint

--- backend/src/main/java/no/ntnu/let/letapi/controller/user/UserController.java
+++ backend/src/main/java/no/ntnu/let/letapi/controller/user/UserController.java
@@ -1,15 +1,25 @@
 package no.ntnu.let.letapi.controller.user;
 
+import com.nimbusds.jose.proc.SecurityContext;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
 import no.ntnu.let.letapi.dto.user.LoginDTO;
 import no.ntnu.let.letapi.dto.user.UserCreationDTO;
 import no.ntnu.let.letapi.dto.user.UserUpdateDTO;
+import no.ntnu.let.letapi.security.AuthenticationService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/user")
+@RequiredArgsConstructor
 public class UserController {
+    private final AuthenticationManager authenticationManager;
+    private final AuthenticationService authenticationService;
     @PostMapping
     public ResponseEntity<Object> createUser(@RequestBody UserCreationDTO userDTO) {
         return ResponseEntity.status(HttpStatus.NOT_IMPLEMENTED).build();
@@ -19,9 +29,17 @@ public class UserController {
         return ResponseEntity.status(HttpStatus.NOT_IMPLEMENTED).build();
     }
 
+    @GetMapping("/session")
+    public ResponseEntity<Object> getUsers() {
+        return ResponseEntity.status(HttpStatus.NOT_IMPLEMENTED).build();
+    }
+
     @PostMapping("/session")
     public ResponseEntity<Object> logInUser(@RequestBody LoginDTO loginDTO) {
-        return ResponseEntity.status(HttpStatus.NOT_IMPLEMENTED).build();
+        Authentication authentication = authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(loginDTO.getEmail(), loginDTO.getPassword()));
+
+        return ResponseEntity.ok(authenticationService.generateToken(authentication));
     }
 
     @DeleteMapping("/session")
