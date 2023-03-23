0d93538d8e889eb2046969bb0738b5beaf9f6ce8
Kaholaz <vkbugge@hotmail.com>
2023-03-23 13:51:56 +0100

    fix: add more allowed origins

--- backend/src/main/java/no/ntnu/let/letapi/security/WebSecurityConfiguration.java
+++ backend/src/main/java/no/ntnu/let/letapi/security/WebSecurityConfiguration.java
@@ -88,7 +88,12 @@ public class WebSecurityConfiguration {
             @Override
             public void addCorsMappings(@NotNull CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:5173")
+                        .allowedOrigins(
+                                "http://127.0.0.1:5173/",
+                                "http://localhost:5173/",
+                                "http://127.0.0.1:8080/",
+                                "http://localhost:8080/"
+                        )
                         .allowedMethods("*")
                         .allowedHeaders("*")
                         .allowCredentials(true);
