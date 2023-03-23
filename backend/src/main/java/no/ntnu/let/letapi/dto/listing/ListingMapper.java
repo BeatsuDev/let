dcec163b92c5b0a142c16c87c72f37c411b7ee67
Kaholaz <vkbugge@hotmail.com>
2023-03-23 19:43:50 +0100

    feat: map to entity references instead of actual enteties

--- backend/src/main/java/no/ntnu/let/letapi/dto/listing/ListingMapper.java
+++ backend/src/main/java/no/ntnu/let/letapi/dto/listing/ListingMapper.java
@@ -5,51 +5,66 @@ import no.ntnu.let.letapi.model.listing.Category;
 import no.ntnu.let.letapi.model.listing.Image;
 import no.ntnu.let.letapi.model.listing.Listing;
 import no.ntnu.let.letapi.model.listing.Location;
+import no.ntnu.let.letapi.repository.listing.CategoryRepository;
+import no.ntnu.let.letapi.repository.listing.ImageRepository;
 import no.ntnu.let.letapi.util.DateUtil;
 import no.ntnu.let.letapi.util.UrlUtil;
 import org.mapstruct.*;
+import org.springframework.beans.factory.annotation.Autowired;
 
 import java.util.Date;
 
 @Mapper(componentModel = "spring", uses = {UserMapper.class}, injectionStrategy = InjectionStrategy.CONSTRUCTOR)
-public interface ListingMapper {
-    Location toLocation(LocationDTO locationDTO);
-    LocationDTO toLocationDTO(Location location);
+public abstract class ListingMapper {
+    @Autowired
+    protected CategoryRepository categoryRepository;
+    @Autowired
+    protected ImageRepository imageRepository;
 
-    Image toImage(Long id);
+    public abstract Location toLocation(LocationDTO locationDTO);
+    public abstract LocationDTO toLocationDTO(Location location);
+
     @Named("toImageUrl")
-    default String toImageUrl(Image image) {
+    public String toImageUrl(Image image) {
         if (image == null) return null;
         return UrlUtil.getImageUrl(image.getFileName());
     }
     @Mapping(target = "url", source = "image", qualifiedByName = "toImageUrl")
-    ImageDTO toImageDTO(Image image);
+    public abstract ImageDTO toImageDTO(Image image);
 
-    default String formatDate(Date date) {
+    String formatDate(Date date) {
         return DateUtil.formatDate(date);
     }
 
-    Category toCategory(CategoryCreationDTO categoryCreationDTO);
-    CategoryDTO toCategoryDTO(Category category);
+    public abstract Category toCategory(CategoryCreationDTO categoryCreationDTO);
+    public abstract CategoryDTO toCategoryDTO(Category category);
 
     @Mappings({
-            @Mapping(target = "category.id", source = "categoryId"),
-            @Mapping(target = "thumbnail", source = "thumbnailId"),
-            @Mapping(target = "gallery", source = "galleryIds")
+            @Mapping(target = "category", expression = "java(categoryRepository.getReferenceById" +
+                    "(listingCreationDTO.getCategoryId()))"),
+            @Mapping(target = "thumbnail", expression = "java(imageRepository.getReferenceById" +
+                    "(listingCreationDTO.getThumbnailId()))"),
+            @Mapping(target = "gallery",
+                    expression = "java(listingCreationDTO.getGalleryIds().stream().map" +
+                            "(imageRepository::getReferenceById).toList())"),
     })
-    Listing toListing(ListingCreationDTO listingCreationDTO);
+    public abstract Listing toListing(ListingCreationDTO listingCreationDTO);
     @Mappings({
-            @Mapping(target = "category.id", source = "categoryId"),
-            @Mapping(target = "thumbnail", source = "thumbnailId"),
-            @Mapping(target = "gallery", source = "galleryIds")
+            @Mapping(target = "category", expression = "java(categoryRepository.getReferenceById" +
+                    "(listingUpdateDTO.getCategoryId()))"),
+            @Mapping(target = "thumbnail", expression = "java(imageRepository.getReferenceById" +
+                    "(listingUpdateDTO.getThumbnailId()))"),
+            @Mapping(target = "gallery",
+                    expression = "java(listingUpdateDTO.getGalleryIds().stream().map" +
+                            "(imageRepository::getReferenceById).toList())"),
     })
-    Listing toListing(ListingUpdateDTO listingUpdateDTO);
+    public abstract Listing toListing(ListingUpdateDTO listingUpdateDTO);
     @Mappings({
             @Mapping(target = "categoryName", source = "category.name"),
             @Mapping(target = "thumbnailUrl", source = "thumbnail", qualifiedByName = "toImageUrl"),
             @Mapping(target = "locationName", source = "location.name")
     })
-    ListingMinimalDTO toListingMinimalDTO(Listing listing);
+    public abstract ListingMinimalDTO toListingMinimalDTO(Listing listing);
     @Mappings({
             @Mapping(target = "categoryName", source = "category.name"),
             @Mapping(target = "thumbnailUrl", source = "thumbnail", qualifiedByName = "toImageUrl"),
@@ -57,5 +72,5 @@ public interface ListingMapper {
             @Mapping(target = "locationName", source = "location.name"),
             @Mapping(target = "seller", source = "seller", qualifiedByName = "toMinimalDTO")
     })
-    ListingFullDTO toListingFullDTO(Listing listing);
+    public abstract ListingFullDTO toListingFullDTO(Listing listing);
 }
