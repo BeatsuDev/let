fac7e2e45fdeec24535d050189d6b3d79cc49b22
Kaholaz <vkbugge@hotmail.com>
2023-03-23 22:12:20 +0100

    feat: add automatic token refresh to jwt filter

--- backend/src/main/java/no/ntnu/let/letapi/security/JwtTokenFilter.java
+++ backend/src/main/java/no/ntnu/let/letapi/security/JwtTokenFilter.java
@@ -5,7 +5,6 @@ import jakarta.servlet.ServletException;
 import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import lombok.RequiredArgsConstructor;
 import org.jetbrains.annotations.NotNull;
 import org.springframework.context.annotation.Lazy;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -36,6 +35,7 @@ public class JwtTokenFilter extends OncePerRequestFilter {
                     .findFirst().ifPresent(cookie -> {
                         try {
                             SecurityContextHolder.getContext().setAuthentication(authenticationService.getAuthentication(cookie.getValue()));
+                            response.addCookie(CookieFactory.getAuthorizationCookie(cookie.getValue()));
                         } catch (Exception e) {
                             SecurityContextHolder.clearContext();
                         }
