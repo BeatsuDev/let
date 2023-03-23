582948a1ed4d2c6da9148b35542128f44b50e2b1
Kaholaz <vkbugge@hotmail.com>
2023-03-23 22:10:54 +0100

    feat: add expiry to cookies

--- backend/src/main/java/no/ntnu/let/letapi/security/CookieFactory.java
+++ backend/src/main/java/no/ntnu/let/letapi/security/CookieFactory.java
@@ -2,12 +2,15 @@ package no.ntnu.let.letapi.security;
 
 import jakarta.servlet.http.Cookie;
 
+import java.time.temporal.ChronoUnit;
+
 public class CookieFactory {
     public static Cookie getAuthorizationCookie(String token) {
         Cookie cookie = new Cookie("Authorization", token);
         cookie.setHttpOnly(true);
         cookie.setSecure(true);
         cookie.setPath("/");
+        cookie.setMaxAge((int) AuthenticationService.TOKEN_DURATION.get(ChronoUnit.SECONDS));
         return cookie;
     }
 }
