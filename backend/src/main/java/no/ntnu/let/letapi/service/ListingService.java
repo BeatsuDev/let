b37dcf0ff6e7ce2e2b4cae646cc445f8b7e5988b
Kaholaz <vkbugge@hotmail.com>
2023-03-23 19:46:17 +0100

    feat added additional logic to create listing service

--- backend/src/main/java/no/ntnu/let/letapi/service/ListingService.java
+++ backend/src/main/java/no/ntnu/let/letapi/service/ListingService.java
@@ -1,27 +1,44 @@
 package no.ntnu.let.letapi.service;
 
+import jakarta.persistence.EntityManager;
+import lombok.RequiredArgsConstructor;
 import no.ntnu.let.letapi.model.listing.Listing;
+import no.ntnu.let.letapi.model.user.User;
 import no.ntnu.let.letapi.repository.listing.ListingRepository;
+import no.ntnu.let.letapi.repository.listing.LocationRepository;
+import no.ntnu.let.letapi.util.DateUtil;
 import no.ntnu.let.letapi.util.ListingFilter;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 
+import java.util.Date;
+
 @Service
+@RequiredArgsConstructor
 public class ListingService {
     private final ListingRepository listingRepository;
-
-    @Autowired
-    ListingService(ListingRepository listingRepository) {
-        this.listingRepository = listingRepository;
-    }
+    private final LocationRepository locationRepository;
+    private final UserService userService;
+    private final EntityManager entityManager;
 
     public Page<Listing> getListings(ListingFilter filter, PageRequest pageRequest) {
         return listingRepository.findAll(filter, pageRequest);
     }
 
+    public Listing createListing(Listing listing, String sellerEmail) {
+        User seller = userService.getUserByEmail(sellerEmail);
+        listing.setSeller(seller);
+        return createListing(listing);
+    }
+
     public Listing createListing(Listing listing) {
+        Date created = DateUtil.getNow();
+        listing.setCreated(created);
+
+        if (listing.getLocation().getId() == null)
+            listing.setLocation(locationRepository.save(listing.getLocation()));
+
         listing = listingRepository.save(listing);
         return listing;
     }
