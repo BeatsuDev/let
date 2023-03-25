099805c5cb2d3242b5d22b992b39efba6ca07df4
Eric Bieszczad-Stie <ericbi@stud.ntnu.no>
2023-03-25 16:09:10 +0100

    feat: initial chat components + chat view

---
+++ frontend/src/components/chat/ChatContainer.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="chat-container-wrapper">
+    Messages here!
+  </div>
+</template>
+
+<script setup lang="ts">
+// Define APIs
+
+// Define props
+
+// Define emits
+
+// Define refs
+
+// Define computed values
+
+// Define callback functions
+
+// Vue hooks
+
+// Other script logic
+
+</script>
+
+<style scoped>
+.chat-container-wrapper {
+  border: 1px solid #e1e1e1;
+  width: 100%;
+  height: 100%;
+}
+</style>
