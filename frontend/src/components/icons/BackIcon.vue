b2f2f1dcad1a036036ea5cd2636bc08ce2617c0c
Arunan Gnanasekaran <arunan2212@gmail.com>
2023-03-25 02:37:30 +0100

    feat: add back button

---
+++ frontend/src/components/icons/BackIcon.vue
@@ -0,0 +1,6 @@
+<template>
+  <svg class="IconSearchDropdownstyles__PreviewIcon-sc-v0m922-1 dZDKpM" viewBox="0 0 24 24">
+    <path d="M0 0h24v24H0z" fill="none"></path>
+    <path d="M11.67 3.87 9.9 2.1 0 12l9.9 9.9 1.77-1.77L3.54 12z"></path>
+  </svg>
+</template>
