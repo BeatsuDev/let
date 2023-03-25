7120ab3621104249e0e4ee1863da81dd7c41c369
Kaholaz <vkbugge@hotmail.com>
2023-03-25 02:47:28 +0100

    feat: add chat repositories

--- backend/src/main/java/no/ntnu/let/letapi/repository/chat/MessageRepository.java
+++ backend/src/main/java/no/ntnu/let/letapi/repository/chat/MessageRepository.java
@@ -1,7 +1,14 @@
 package no.ntnu.let.letapi.repository.chat;
 
+import no.ntnu.let.letapi.model.chat.Chat;
 import no.ntnu.let.letapi.model.chat.Message;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+import java.util.Optional;
+
 public interface MessageRepository extends JpaRepository<Message, Long> {
+    List<Message> getMessagesByChatOrderByTimestampDesc(Chat chat);
+
+    Optional<Message> findFirstByChatOrderByTimestampDesc(Chat chat);
 }
