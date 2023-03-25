ec8a1cfc21edf32f9ea7f25b30929457d59cc0b7
Eric Bieszczad-Stie <ericbi@stud.ntnu.no>
2023-03-25 21:25:09 +0100

    feat: connected chat to backend (very buggy, initial commit)

--- frontend/src/views/ChatsListView.vue
+++ frontend/src/views/ChatsListView.vue
@@ -1,26 +1,45 @@
 <template>
-  <main class="chat-view-wrapper">
-    <ChatList class="chat-list"/>
-    <ChatContainer class="chat-container" />
+  <main>
+    <NavigationDrawer v-model="collapsed">
+      <ChatList class="chat-list" @chat-selected="loadChat"/>
+    </NavigationDrawer>
+    <MainContainer class="chat-view-wrapper" :collapse="collapsed">
+      <ChatContainer class="chat-container" :chat="selectedChat"/>
+    </MainContainer>
   </main>
 </template>
 
 <script setup lang="ts">
+import { ref } from "vue";
+
+import { ChatApi } from "@/services/index";
+import type { ChatOverview, Chat } from "@/types/chat";
+
 import ChatList from "@/components/chat/ChatList.vue";
 import ChatContainer from "@/components/chat/ChatContainer.vue";
+import MainContainer from "@/components/containers/MainContainer.vue";
+import NavigationDrawer from "@/components/navigations/NavigationDrawer.vue";
 
 // Define APIs
-// const userApi = new UserApi();
+const chatApi = new ChatApi();
 
 // Define props
 
 // Define emits
 
 // Define refs
+const selectedChat = ref<Chat | null>(null);
+const collapsed = ref(false);
 
 // Define computed values
 
 // Define callback functions
+async function loadChat(chat: ChatOverview) {
+  console.log("Loading chat with id: " + chat.id);
+
+  const response = await chatApi.getChat(chat.id);
+  selectedChat.value = response.data as Chat;
+}
 
 // Other script logic
 
