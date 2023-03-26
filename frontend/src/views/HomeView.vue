1055cef18704a82fbe99b19c873ea9082f36387a
Arunan Gnanasekaran <arunan2212@gmail.com>
2023-03-26 22:17:05 +0200

    refac: add different messages on home view

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -5,7 +5,7 @@
     </NavigationDrawer>
     <MainContainer :collapsed="collapsed">
       <AlertBox v-if="errorMessage" :message="errorMessage" type="error" />
-      <h1 class="text-one-line" style="height: 4rem">Hva leter du etter i dag?</h1>
+      <h1 class="text-one-line" style="height: 4rem; transition: 0.3s all">{{ message }}</h1>
       <PaginationView
         v-model="listingFilter.page"
         :total-pages="totalPages"
@@ -20,7 +20,7 @@
   </main>
 </template>
 <script setup lang="ts">
-import { computed, ref, watch } from "vue";
+import { computed, onMounted, ref, watch } from "vue";
 import { ListingFilter } from "@/types/listing";
 import NavigationDrawer from "@/components/navigations/NavigationDrawer.vue";
 import type { Category, InlineResponse200 } from "@/services/models";
@@ -29,7 +29,6 @@ import { ListingsApi } from "@/services/apis/listings-api";
 import { CategoryApi } from "@/services/apis/category-api";
 import PaginationView from "@/components/paginations/PaginationContainer.vue";
 import ListingScrollPane from "@/components/listings/ListingScrollPane.vue";
-import runAxios from "@/services/composable";
 import MainContainer from "@/components/containers/MainContainer.vue";
 import ListingsNotFound from "@/components/listings/NoListingsFound.vue";
 import AlertBox from "@/components/dialogs/AlertBox.vue";
@@ -45,6 +44,15 @@ const loading = ref(false);
 const errorMessage = ref("" as string);
 const collapsed = ref(false);
 const categories = ref([] as Category[]);
+const message = ref("");
+
+const messages = [
+  "Hva leter du etter i dag? ☘️",
+  "Velkommen til  ̶F̶I̶N̶N̶.̶n̶o̶  letno!",
+  "På tide å finne drømmetingen! ",
+  "Slår du gull i dag? 🪙",
+  "Velkommen tilbake 👋",
+];
 
 fetchListings();
 categoryApi.getCategories().then((response) => {
@@ -95,6 +103,9 @@ function fetchListings() {
 }
 
 // Vue hooks
+onMounted(() => {
+  message.value = messages[Math.floor(Math.random() * messages.length)];
+});
 watch(
   listingFilter,
   () => {
