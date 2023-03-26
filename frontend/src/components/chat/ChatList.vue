36da4cf223c53126437b46cb2d35ffe75b536ac5
Kaholaz <vkbugge@hotmail.com>
2023-03-26 18:46:52 +0200

    refac: use swagger defined types instead of self defined types

--- frontend/src/components/chat/ChatList.vue
+++ frontend/src/components/chat/ChatList.vue
@@ -5,7 +5,12 @@
     <AlertBox :message="'Feil ved henting av chatter: ' + chatFetchError.message" />
   </div>
   <div v-else class="chat-list-wrapper">
-    <div class="chat" v-for="chat in chats" @click="emit('chat-selected', chat)">
+    <div
+      class="chat"
+      v-for="(chat, index) in chats"
+      :key="index"
+      @click="emit('chat-selected', chat)"
+    >
       <h3>[{{ chat.id }}]: {{ chat.listing.title }}</h3>
       <p>{{ chat.lastMessage?.content || "(ny chat)" }}</p>
     </div>
@@ -14,22 +19,17 @@
 
 <script setup lang="ts">
 import runAxios from "@/services/composable";
-import { ChatApi } from "@/services/index";
-import type { ChatOverview } from "@/types/chat";
-import type { AxiosResponse } from "axios";
+import {ChatApi, ChatMinimal} from "@/services/index";
 import AlertBox from "../dialogs/AlertBox.vue";
 
 // Define APIs
 const chatApi = new ChatApi();
 
 // Define props
-const { data: chats, error: chatFetchError } = runAxios<ChatOverview[]>(
-  chatApi.getChats() as Promise<AxiosResponse<ChatOverview[]>>
-);
 
 // Define emits
 const emit = defineEmits<{
-  (event: "chat-selected", chat: ChatOverview): void;
+  (event: "chat-selected", chat: ChatMinimal): void;
 }>();
 
 // Define refs
@@ -39,6 +39,7 @@ const emit = defineEmits<{
 // Define callback functions
 
 // Other script logic
+const { data: chats, error: chatFetchError } = runAxios(chatApi.getChats());
 </script>
 
 <style scoped>
