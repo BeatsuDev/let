fc50a280df128afeae0b520ce75d8bd2ec05a25a
Eric Bieszczad-Stie <ericbieszczad@gmail.com>
2023-03-26 18:16:00 +0200

    fix: fixed register form

--- frontend/src/views/RegisterView.vue
+++ frontend/src/views/RegisterView.vue
@@ -28,14 +28,8 @@ const userApi = new UserApi();
 const sessionStore = useSessionStore();
 
 //Define refs
-const userEdit = ref({
-  firstName: "",
-  lastName: "",
-  email: "",
-  password: "",
-} as UserBody);
+const user = ref({} as UserBody);
 const errorMessage = ref("");
-const user = sessionStore.getUser() || ({} as UserBody);
 
 const responses = [
   "På tide å finne ting 🔍",
@@ -49,7 +43,7 @@ const randomResponse = responses[Math.floor(Math.random() * responses.length)];
 //Define callbacks
 function register() {
   userApi
-    .createUser(userEdit.value)
+    .createUser(user.value)
     .then((response) => {
       sessionStore.authenticate(response.data);
       router.push("/");
