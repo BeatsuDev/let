97c0857483faece5b644dc5758edf6d01bd4a2b7
Kaholaz <vkbugge@hotmail.com>
2023-03-27 19:13:18 +0200

    refac: change how chat mapper tests are set up

--- backend/src/test/java/no/ntnu/let/letapi/dto/chat/TestChatMapper.java
+++ backend/src/test/java/no/ntnu/let/letapi/dto/chat/TestChatMapper.java
@@ -2,48 +2,30 @@ package no.ntnu.let.letapi.dto.chat;
 
 import no.ntnu.let.letapi.model.chat.Chat;
 import no.ntnu.let.letapi.model.chat.Message;
-import no.ntnu.let.letapi.model.listing.Listing;
-import org.aspectj.lang.annotation.Before;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
-
-import java.util.List;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.junit.MockitoJUnitRunner;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+@RunWith(MockitoJUnitRunner.class)
 public class TestChatMapper {
-    private ChatMapper chatMapper = new ChatMapperImpl();
-    private ChatCreationDTO chatCreationDTO = new ChatCreationDTO();
-    private ChatMinimalDTO chatMinimalDTO = new ChatMinimalDTO();
-    private ChatFullDTO chatFullDTO = new ChatFullDTO();
-    private Chat chat = new Chat();
-    private MessageCreationDTO messageCreationDTO = new MessageCreationDTO();
-    private MessageDTO messageDTO = new MessageDTO();
-    private Message message = new Message();
+    private final ChatMapper chatMapper = new ChatMapperImpl();
+    private final ChatCreationDTO chatCreationDTO = new ChatCreationDTO();
+    private final MessageCreationDTO messageCreationDTO = new MessageCreationDTO();
+    private final MessageDTO messageDTO = new MessageDTO();
+    private final Message message = new Message();
 
-    @BeforeEach
-    public void resetChatCreationDTO() {
+    @Before
+    public void setUp() {
         chatCreationDTO.setListingId(1L);
-    }
-
-
-    @BeforeEach
-    public void resetChat() {
-        chat.setId(1L);
-        chat.setListing(new Listing());
-    }
-
-    @BeforeEach
-    public void resetMessageCreationDTO() {
         messageCreationDTO.setContent("content");
-    }
-
-    @BeforeEach
-    public void resetMessage() {
         message.setId(1L);
         message.setContent("content");
+        chatCreationDTO.setListingId(1L);
+        messageDTO.setContent("content");
     }
-
 
     @Test
     public void testFromChatCreationDTO() {
