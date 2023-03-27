74aea3d8d056260a5afa76d7e5b69ea7f832efb4
Eric Bieszczad-Stie <ericbieszczad@gmail.com>
2023-03-27 11:23:28 +0200

    chore: attempts to get cypress coverage working

---
+++ frontend/nyc.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+    extends: '@istanbuljs/nyc-config-typescript',
+    all: true,
+    extension: [
+        '.vue',
+        '.ts',
+    ],
+    include: [
+        'src/**/*.{vue,ts}',
+    ],
+}
