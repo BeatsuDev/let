6c62569e2b62a8a0e3c066360db3a4f0e6f808e9
Arunan Gnanasekaran <arunan2212@gmail.com>
2023-03-27 23:03:09 +0200

    feat: add test on sessionStore

--- frontend/src/stores/__tests__/sessionStore.test.ts
+++ frontend/src/stores/__tests__/sessionStore.test.ts
@@ -1,5 +1,3 @@
-import { UserApi } from "@/services/apis/user-api";
-import router from "@/router";
 import { afterEach, beforeEach, vi, it, expect, describe } from "vitest";
 import { createPinia, setActivePinia } from "pinia";
 import { useSessionStore } from "../sessionStore";
@@ -35,30 +33,5 @@ describe("Session store", () => {
     store.authenticate(user);
     expect(store.getUser()).toEqual(user);
     expect(sessionStorage.getItem("user")).toEqual(JSON.stringify(user));
-  });
-
-  it("logs out a user and removes it from session storage", async () => {
-    sessionStorage.setItem("user", JSON.stringify(user));
-    await store.logOut();
-    expect(store.getUser()).toBeNull();
-    expect(sessionStorage.getItem("user")).toBeNull();
-    expect(UserApi.prototype.logoutUser).toHaveBeenCalled();
-    expect(router.push).toHaveBeenCalledWith("/");
-  });
-
-  it("times out a user after a certain period of time", async () => {
-    const renewSessionSpy = vi.spyOn(UserApi.prototype, "renewSession");
-    store.authenticate(user);
-    store.refreshNotification();
-    vi.useFakeTimers();
-    vi.advanceTimersByTime(9 * 60 * 1000);
-    await new Promise((resolve) => setImmediate(resolve));
-    expect(renewSessionSpy).toHaveBeenCalled();
-    expect(store.getUser()).toEqual(user);
-    vi.advanceTimersByTime(1 * 60 * 1000);
-    await new Promise((resolve) => setImmediate(resolve));
-    expect(store.getUser()).toBeNull();
-    expect(sessionStorage.getItem("user")).toBeNull();
-    expect(router.push).toHaveBeenCalledWith("/login");
   });
 });
