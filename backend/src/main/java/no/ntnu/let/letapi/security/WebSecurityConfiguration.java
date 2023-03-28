2ebd7b7003b92243f14d61129b1ac724edb31575
Kaholaz <vkbugge@hotmail.com>
2023-03-28 23:06:12 +0200

    fix: add production frontend to allowed origins

--- backend/src/main/java/no/ntnu/let/letapi/security/WebSecurityConfiguration.java
+++ backend/src/main/java/no/ntnu/let/letapi/security/WebSecurityConfiguration.java
@@ -124,8 +124,8 @@ public class WebSecurityConfiguration {
                                 "http://127.0.0.1:5173/",
                                 "http://localhost:5173/",
                                 "http://127.0.0.1:8080/",
-                                "http://localhost:8080/"
-                                "http://letno.no"
+                                "http://localhost:8080/",
+                                "https://letno.no/"
                         )
                         .allowedMethods("*")
                         .allowedHeaders("*")
