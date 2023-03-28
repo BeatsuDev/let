527452e7474d1e11d8803dd59409d88f30befac4
Arunan Gnanasekaran <arunan2212@gmail.com>
2023-03-28 13:39:55 +0200

    feat: add letno.no on allowed origins

--- backend/src/main/java/no/ntnu/let/letapi/security/WebSecurityConfiguration.java
+++ backend/src/main/java/no/ntnu/let/letapi/security/WebSecurityConfiguration.java
@@ -125,6 +125,7 @@ public class WebSecurityConfiguration {
                                 "http://localhost:5173/",
                                 "http://127.0.0.1:8080/",
                                 "http://localhost:8080/"
+                                "http://letno.no"
                         )
                         .allowedMethods("*")
                         .allowedHeaders("*")
